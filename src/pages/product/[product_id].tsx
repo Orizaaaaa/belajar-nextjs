f3d50e2d905fa8571b79fbc102d21e37e9613d5e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-18 11:57:57 +0700

    create serverside dynamic routes

--- src/pages/product/[product_id].tsx
+++ src/pages/product/[product_id].tsx
@@ -3,22 +3,47 @@ import { useRouter } from 'next/router'
 import useSWR from 'swr';
 import { fetcher } from '@/lib/swr/swr';
 import DetailProduct from '@/views/DetailProduct';
-
-function DetailProductPage() {
+type Props = {
+    id: string
+    name: string
+    price: number
+    image: string
+    category: string
+}
+function DetailProductPage({ product }: { product: Props }) {
     const { query } = useRouter()
-
-    const { data, error, isLoading } = useSWR(
-        `/api/product/${query.product_id}`,
-        fetcher
-    );
+    // client side
+    // const { data, error, isLoading } = useSWR(
+    //     `/api/product/${query.product_id}`,
+    //     fetcher
+    // );
 
     return (
+
         <div>
-            <h1>DetailProductPage</h1>
-            <DetailProduct product={isLoading ? [] : data.data} />
+            {/* client side */}
+            {/* <h1>DetailProductPage</h1>
+            <DetailProduct product={isLoading ? [] : data.data} /> */}
+
+            {/* server side */}
+            <DetailProduct product={product} />
         </div>
+
+
+
 
     )
 }
 
 export default DetailProductPage
+
+export async function getServerSideProps({ params }: { params: { product_id: string } }) {
+    console.log(params.product_id);
+    const res = await fetch(`http://localhost:3000/api/product/${params.product_id}`);
+    const response = await res.json();
+    return {
+        props: {
+            product: response.data
+        }
+    };
+}
