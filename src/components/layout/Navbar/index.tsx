f4d05f5e654009569fe411e796be223cbcb5f2d6
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-24 10:59:51 +0700

    create next auth

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,9 +1,21 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
 function Navbar() {
+    const { data }: any = useSession()
+
     return (
-        <div className='navbar'>
+        <div className='navbar flex justify-between'>
             <div> Navbar</div>
+            <div className="full-name">
+                {data ? data.user.fullName : "Guest"}
+            </div>
+            {data ? (
+                <button onClick={() => signOut()} >SiGN OUT</button>
+            ) : (
+                <button onClick={() => signIn()} >SiGN IN</button>
+            )
+            }
         </div>
     )
 }
