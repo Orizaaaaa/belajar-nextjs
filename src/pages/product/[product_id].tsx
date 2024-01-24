197dcbab962dff7f6d141554875c9645def84c7f
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-24 07:45:20 +0700

    fixing all

--- src/pages/product/[product_id].tsx
+++ src/pages/product/[product_id].tsx
@@ -12,6 +12,8 @@ type Props = {
 }
 function DetailProductPage({ product }: { product: Props }) {
     const { query } = useRouter()
+    console.log(query);
+
     // client side
     // const { data, error, isLoading } = useSWR(
     //     `/api/product/${query.product_id}`,
@@ -38,9 +40,9 @@ function DetailProductPage({ product }: { product: Props }) {
 export default DetailProductPage
 
 export async function getServerSideProps({ params }: { params: { product_id: string } }) {
-    console.log(params.product_id);
     const res = await fetch(`http://localhost:3000/api/product/${params.product_id}`);
     const response = await res.json();
+
     return {
         props: {
             product: response.data
